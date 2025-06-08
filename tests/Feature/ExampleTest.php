2a9470dc84504815b23daef045ea985604bf45a3
Raidflo <84374632+Raidflo@users.noreply.github.com>
2025-06-08 18:28:21 +0200

    Update ExampleTest.php

--- tests/Feature/ExampleTest.php
+++ tests/Feature/ExampleTest.php
@@ -1,7 +1,19 @@
 <?php
 
-test('returns a successful response', function () {
+test('returns a successful response /', function () {
     $response = $this->get('/');
+
+    $response->assertStatus(200);
+});
+
+test('returns a successful response /cart', function () {
+    $response = $this->get('/cart');
+
+    $response->assertStatus(200);
+});
+
+test('returns a successful response /product/1', function () {
+    $response = $this->get('/product/1');
 
     $response->assertStatus(200);
 });
