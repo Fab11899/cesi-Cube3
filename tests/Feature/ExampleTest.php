0fa3a1dc39212d7680562afba14dc32844995ff6
Raidflo <84374632+Raidflo@users.noreply.github.com>
2025-06-08 20:03:50 +0200

    Update ExampleTest.php

--- tests/Feature/ExampleTest.php
+++ tests/Feature/ExampleTest.php
@@ -47,27 +47,3 @@ test('returns a successful response api/products', function () {
 
     $response->assertStatus(200);
 });
-
-test('returns a successful response api/product/1', function () {
-    $response = $this->get('api/product/1');
-
-    $response->assertStatus(200);
-});
-
-test('returns a successful response api/product/2', function () {
-    $response = $this->get('api/product/2');
-
-    $response->assertStatus(200);
-});
-
-test('returns a successful response api/product/3', function () {
-    $response = $this->get('api/product/3');
-
-    $response->assertStatus(200);
-});
-
-test('returns a successful response api/product/4', function () {
-    $response = $this->get('api/product/4');
-
-    $response->assertStatus(200);
-});
